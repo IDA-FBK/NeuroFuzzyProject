f39d767aca4560cfcef64413be5d315bd90cd417
Giovanni Valer <giovanni.valer@studenti.unitn.it>
2024-11-20 16:02:51 +0100

    feat: selection with crossover

---
+++ models/crossover.py
@@ -0,0 +1,12 @@
+def crossover(parent1, parent2):
+    """
+    Perform crossover between two individuals.
+    This works by swapping some parameters of the two fuzzy neural networks.
+    The parameters we consider are neuron weights and gaussian parameters.
+    """
+    # TODO: Implement crossover.
+    # NOTE: Parameters are: parent.neuron_weights, parent.V, parent.mf_params
+    # parent.mf_params = [
+        # {"centers": [], "sigmas": []} for _ in range(num_features)
+    # ]
+
---
+++ models/selection.py
@@ -0,0 +1,44 @@
+import random
+from models.crossover import *
+
+
+def selection(population, selection_mu, selection_lambda, mutation_rate, selection_strategy):
+    """
+    Select the best individuals from the population, eventually with mutation.
+    population: list of Individual
+    selection_mu: int, number of individuals to select from the population
+    selection_lambda: int, number of individuals to generate
+    mutation_rate: float, probability of mutation
+    selection_strategy: str, either "plus" or "comma"
+    """
+    # Generate new individuals
+    offspring = generate_offspring(population, selection_lambda, mutation_rate)
+    # Select the best individuals
+    if selection_strategy == "plus":
+        offspring += population
+    offspring.sort(key=lambda x: x.fitness, reverse=True)
+    return offspring[:selection_mu]
+
+
+def generate_offspring(parents, selection_lambda, mutation_rate):
+    """
+    Generate new individuals from the parents, with probability of parent selection proportional to fitness.
+    parents: list of Individual
+    selection_lambda: int, number of individuals to generate
+    mutation_rate: float, probability of mutation
+    """
+    # Compute the probability of selecting each parent
+    fitness_sum = sum(p.fitness for p in parents)
+    parent_probabilities = [p.fitness / fitness_sum for p in parents]
+    # Generate offspring
+    offspring = []
+    for _ in range(selection_lambda):
+        # Select two parents
+        parent1 = random.choices(parents, weights=parent_probabilities)[0]
+        parent2 = random.choices(parents, weights=parent_probabilities)[0]
+        # Crossover
+        child = crossover(parent1, parent2)
+        # Mutation
+        child = child.mutate(mutation_rate=mutation_rate)
+        offspring.append(child)
+    return offspring
