acdceba04d3da8024d86449b1728e977f255ffc8
Giovanni Valer <giovanni.valer@studenti.unitn.it>
2024-11-21 17:44:33 +0100

    feat: crossover

--- models/crossover.py
+++ models/crossover.py
@@ -1,12 +1,32 @@
-def crossover(parent1, parent2):
+import random
+
+SWAP4ALL_MFS = True # If True, swap all gaussian parameters of a feature, for all its membership functions.
+
+
+def crossover(parent1, parent2, crossover_rate=0.1):
     """
     Perform crossover between two individuals.
     This works by swapping some parameters of the two fuzzy neural networks.
-    The parameters we consider are neuron weights and gaussian parameters.
+    The parameters we consider are parent.neuron_weights, parent.V and gaussian parameters (parent.mf_params).
+    crossover_rate: float, probability of swapping each parameter
     """
-    # TODO: Implement crossover.
-    # NOTE: Parameters are: parent.neuron_weights, parent.V, parent.mf_params
-    # parent.mf_params = [
-        # {"centers": [], "sigmas": []} for _ in range(num_features)
-    # ]
+    child = parent1.copy()
+    # Swap neuron weights
+    for i in range(len(child.neuron_weights)):
+        if random.random() < crossover_rate:
+            child.neuron_weights[i] = parent2.neuron_weights[i]
+    # Swap last layer weights
+    if random.random() < crossover_rate:
+        child.V = parent2.V
+    # Swap gaussian parameters
+    for feature_index in range(len(child.mf_params)):
+        if SWAP4ALL_MFS:
+            if random.random() < crossover_rate:
+                child.mf_params[feature_index] = parent2.mf_params[feature_index]
+        else:
+            for mf_index in range(child.num_mfs):
+                if random.random() < crossover_rate:
+                    child.mf_params[feature_index]["centers"][mf_index] = parent2.mf_params[feature_index]["centers"][mf_index]
+                    child.mf_params[feature_index]["sigmas"][mf_index] = parent2.mf_params[feature_index]["sigmas"][mf_index]
+    return child
 
